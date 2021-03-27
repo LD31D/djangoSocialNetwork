fd0b826042d6c663291ab3ae4e36407bfc1b548b
Artem LD31D <artem.12m21@gmail.com>
2021-03-27 15:52:30 +0200

    Added services to working with likes

--- core/serializers.py
+++ core/serializers.py
@@ -1,14 +1,16 @@
 from rest_framework import serializers
 
+from . import services
 from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+	is_fan = serializers.SerializerMethodField('get_is_fan', read_only=True)
 	author = serializers.SerializerMethodField('get_author', read_only=True)
 
 	class Meta:
 		model = Post
-		fields = ('id', 'body', 'created', 'author', 'total_likes')
+		fields = ('id', 'body', 'created', 'author', 'total_likes', 'is_fan')
 
 		extra_kwargs = {
 			'author': {'required': False},
@@ -20,6 +22,10 @@ class PostSerializer(serializers.ModelSerializer):
 				'username': obj.author.username,
 				'is_active': obj.author.is_active
 			}
+
+	def get_is_fan(self, obj):
+		user = self.context['request'].user
+		return services.is_fan(obj, user)
 
 	def create(self, validated_data):
 		validated_data['author'] = self.context['request'].user
---
+++ core/services.py
@@ -0,0 +1,19 @@
+from .models import Like
+
+
+def add_like(post_obj, user_ob):
+	like, is_created = Like.objects.get_or_create(post=post_obj, user=user_obj)
+
+	return like
+
+
+def remove_like(post_obj, user_ob):
+	Like.objects.filter(post=post_obj, user=user_obj).delete()
+
+
+def is_fan(post_obj, user_obj):
+	if not user_obj.is_authenticated:
+		return False
+
+	likes = Like.objects.filter(post=post_obj, user=user_obj)
+	return likes.exists()
