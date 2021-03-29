ad4cfe0cc359bd79329b15a3d0c350e52692488b
Artem LD31D <artem.12m21@gmail.com>
2021-03-29 09:31:48 +0300

    Added README.md

---
+++ README.md
@@ -0,0 +1,26 @@
+## djangoSocialNetwork
+
+### [Test task for `StarNavi`](https://drive.google.com/file/d/1jo2O5Y5ImMA6MGnV1jfgxeb-9Q2fJu6m/view)
+
+---
+
+#### Start Django API Server
+>  pip install -r requrements.txt
+
+>  python manage.py makemigrations
+
+>  python manage.py migrate
+
+>  python manage.py runserver
+
+---
+
+#### Start Automated Bot
+> cd automated_bot
+
+> python main.py
+
+
+### P.S
+All bugs found in the program are features and are provided by the developer !!! 
+
--- core/api/mixins.py
+++ core/api/mixins.py
@@ -6,17 +6,15 @@ from .. import services
 
 class LikedMixin:
 
-    @action(methods=['POST', 'GET'], detail=True)
-    def like(self, request, pk=None):
-        post_obj = self.get_object()
-        services.add_like(post_obj, request.user)
+	@action(methods=['POST'], detail=True)
+	def like(self, request, pk=None):
+		post_obj = self.get_object()
+		services.add_like(post_obj, request.user)
+		return Response({'ok': True})
 
-        return Response({'ok': True})
+	@action(methods=['POST'], detail=True)
+	def unlike(self, request, pk=None):
+		post_obj = self.get_object()
+		services.remove_like(post_obj, request.user)
 
-    @action(methods=['POST', 'GET'], detail=True)
-    def unlike(self, request, pk=None):
-        post_obj = self.get_object()
-        services.remove_like(post_obj, request.user)
-
-
-        return Response({'ok': True})
+		return Response({'ok': True})
